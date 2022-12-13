18472e325daedc95b39c3beee174fdf191f1be09
susui <2559@qq.com>
2022-12-13 20:06:17 +0800

    Input parameter modification

--- utils/combine_jsons.py
+++ utils/combine_jsons.py
@@ -13,7 +13,8 @@
 # DM19-0988                                                                                                                                                         #
 #####################################################################################################################################################################
 
-import json 
+import json
+
 
 def combine_output(pred_polygons, pred_classification, output_file):
     """
@@ -27,25 +28,25 @@ def combine_output(pred_polygons, pred_classification, output_file):
         "features": {
             "lng_lat": [],
             "xy": []
-        }, 
+        },
         "metadata": {
             "sensor": "",
             "provider_asset_type": "",
             "gsd": 0,
-            "capture_date": "", 
-            "off_nadir_angle": 0, 
-            "pan_resolution": 0, 
-            "sun_azimuth": 0, 
-            "sun_elevation": 0, 
-            "target_azimuth": 0, 
-            "disaster": "", 
-            "disaster_type": "", 
-            "catalog_id": "", 
-            "original_width": 0, 
-            "original_height": 0, 
-            "width": 0, 
-            "height": 0, 
-            "id": "", 
+            "capture_date": "",
+            "off_nadir_angle": 0,
+            "pan_resolution": 0,
+            "sun_azimuth": 0,
+            "sun_elevation": 0,
+            "target_azimuth": 0,
+            "disaster": "",
+            "disaster_type": "",
+            "catalog_id": "",
+            "original_width": 0,
+            "original_height": 0,
+            "width": 0,
+            "height": 0,
+            "id": "",
             "img_name": ""
         }
     }
@@ -53,21 +54,22 @@ def combine_output(pred_polygons, pred_classification, output_file):
     # Open the classification json 
     with open(pred_classification) as labels:
         label_json = json.load(labels)
-        
-        # Open the localization json 
+
+        # Open the localization json
         with open(pred_polygons) as polys:
             poly_json = json.load(polys)
 
-            # Match UUIDs from the two jsons and combine in output_json skeleton 
+            # Match UUIDs from the two jsons and combine in output_json skeleton
             for p in poly_json['features']['xy']:
                 p['properties']['subtype'] = label_json[p['properties']['uid']]
                 output_json['features']['xy'].append(p)
-    
-    # Finally save out the combined json file 
-    with open(output_file, 'w') as out: 
+
+    # Finally save out the combined json file
+    with open(output_file, 'w') as out:
         json.dump(output_json, out)
 
-if __name__ == '__main__': 
+
+if __name__ == '__main__':
     import argparse
 
     # Parse command line arguments
@@ -76,19 +78,22 @@ if __name__ == '__main__':
         """combine_jsons.py: combines the outputs of localization and classification inference into a single output json"""
     )
     parser.add_argument('--polys',
-                        required=True,
+                        required=False,
+                        default="/mnt/disk3/zwy/zwy/sematicseg/xView2_baseline-master/spacenet/infer_out/1.json",
                         metavar='/path/to/input/polygons.json',
                         help="Full path to the json from polygonize.py")
     parser.add_argument('--classes',
-                        required=True,
+                        required=False,
+                        default="/mnt/disk3/zwy/zwy/sematicseg/xView2_baseline-master/model/infer_data/out_json/1.json",
                         metavar='/path/to/classifications.json',
                         help="Full path to the json from tensor_inf.py"
-    )
+                        )
     parser.add_argument('--output',
-                        required=True,
+                        required=False,
+                        default='/mnt/disk3/zwy/zwy/sematicseg/xView2_baseline-master/model/infer_data/out_pred/1.json',
                         metavar='/path/to/pred.json',
                         help="Full path to save the final single output file to"
-    )
+                        )
 
     args = parser.parse_args()
 
